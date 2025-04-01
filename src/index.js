ea7f62547d4ed98f9f7d041884e8d1cccaed44c7
Kashii-00 <165863712+Kashii-00@users.noreply.github.com>
2025-04-01 15:35:53 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MaintenancePage from './MaintenancePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MaintenancePage />
   </React.StrictMode>
 );
 
